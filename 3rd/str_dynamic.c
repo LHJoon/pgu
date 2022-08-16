95fee0251d32b6b01277629523e5a0ddbdd3c994
korjwl1 <72062804+korjwl1@users.noreply.github.com>
2022-08-17 00:12:20 +0900

    dynamic str 문제 추가

---
+++ 3rd/str_dynamic.c
@@ -0,0 +1,26 @@
+// 필요한 라이브러리 추가
+
+// toCopy의 내용을 깊은 복사하여 반환한다. 
+// Input: String to be copied / Output: Pointer of copied String
+char *dynamic_strcpy(const char *toCopy) {
+
+}
+
+// str1와 str2의 내용을 그대로 이어붙인 새로운 문자열을 반환한다.
+// Input: Two String to be concatenated / Output: Pointer of concatenated String
+char *dynamic_strcat(const char *str1, const char *str2) {
+
+}
+
+int main() {
+    // dynamic_strcpy의 입력값이 될 문자열
+    char *toCopy = "What'll you do when you get lonely";
+
+    // dynamic_strcat의 입력값이 될 문자열 2개
+    char *str1 = "And nobody's waiting by your side?";
+    char *str2 = "You've been running and hiding much too long";
+
+    // dynamic_strcpy와 dynamic_strcat을 각각 실행하고 출력하며 검증 등의 기타 필요한 작업을 진행한다.
+
+    return 0;
+}
